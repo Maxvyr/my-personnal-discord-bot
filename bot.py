cac5a27f6bb82ecb020c5e5fe797e6c45bbe1a11
Maxvyr <maxvid30@gmail.com>
2022-02-04 10:06:18 +0100

    :sparkles: change command launch bot

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-import discord
 import os
 import datetime
 from nextcord.ext import commands
@@ -9,39 +8,18 @@ from utils import your_ip
 load_dotenv()
 
 # prefix de commande du bot
-bot = commands.Bot(command_prefix="!")
-
-default_intents = discord.Intents.default()
-default_intents.members = True
-
-client = discord.Client(intents=default_intents)
-
-
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-    
-@client.event
-async def on_message(message):
-    if message.content == "Ping":
-        await message.channel.send("Pong")
-    
-@client.event
-async def on_member_join(member):
-    channel_arrive = client.get_channel(696992636485238787)
-    channel_arrive.send(f"Bienvenue à {member.display_name} ! ")
-    print(f"L'utilisateur {member.display_name} à rejoint")
+bot = commands.Bot(command_prefix=">")
 
 @bot.event
 async def on_ready():
     print("Bot Ready")
 
 # définir la command d'un bot avec le décorateur
-@bot.command(name="del")
+@bot.command(name="clear")
 async def delete(ctx, number:int=-1):
     if(number == -1.0):
         await ctx.channel.send(f"Vous avez oubliez de rentrer le nombre de messaque je dois supprimer")
-        await ctx.channel.send(f"example: **!del 2** ")
+        await ctx.channel.send(f"example: **>clear 2** ")
         await ctx.channel.send(f"Si vous voulez **supprimer** les **2 messages précédents**")
     else:
         messages = await ctx.channel.history(limit=number + 1).flatten()
